b1866d8ebbdef33e2206768229d6504a6fce3672
NoamRa <noamraby@gmail.com>
2019-03-02 22:23:21 +0200

    remove junk from Home

--- client/src/Components/Statics/Home.jsx
+++ client/src/Components/Statics/Home.jsx
@@ -1,34 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { getAllImages } from "../../Logic/gallery";
+import React from "react";
 
 const Home = () => {
 
-  const [ images, setImages ] = useState([]);
-  
-  const fetchImages = async () => {
-    const images = await getAllImages();
-    setImages(images);
-  };
-
-  useEffect(() => { fetchImages() }, []);
-
   return (
     <div>
-      {console.log('home page')}
-      {console.log(images)}
-      Home Page
-      {/* <button onClick={getAllImages}>test</button> */}
-      <ul>
-        {
-          images.map(image => (
-            <li key={image}>
-              <a href={image}>
-                <img src={image} alt={image}/>
-              </a>
-            </li>
-          ))
-        }
-      </ul>
+        Home Page
     </div>
   )
 

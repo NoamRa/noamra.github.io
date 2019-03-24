fa82e96c3751efec2c25a0165fe0a498ce0fc55f
NoamRa <noamraby@gmail.com>
2019-03-24 13:15:51 +0200

    cosmetic

--- client/src/Conf/api.ts
+++ client/src/Conf/api.ts
@@ -1,7 +1,7 @@
 export const getGalleryItemsURL: string = (
   location.hostname === "localhost" ?
-  "https://ld7cjn7mag.execute-api.us-east-1.amazonaws.com/dev/gallery/" :
-  "https://ld7cjn7mag.execute-api.us-east-1.amazonaws.com/prod/gallery/"
+  "https://ld7cjn7mag.execute-api.us-east-1.amazonaws.com/dev/gallery" :
+  "https://ld7cjn7mag.execute-api.us-east-1.amazonaws.com/prod/gallery"
 );
 
 export const galleryAssets: string = "dcz2v9kr60o99.cloudfront.net";
